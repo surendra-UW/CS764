70197eaf1ed0f20b02541b4045a04ca7f9ddced4
Srihari <srihari961112@gmail.com>
2023-11-08 15:37:55 -0600

    Added string header in defs.h

--- Buffer.cpp
+++ Buffer.cpp
@@ -3,42 +3,55 @@
 #include "RecordStructure.h"
 using namespace std;
 
-Buffer::Buffer(int bufferSize, bool inputBuffer, string hierarchy) : size(bufferSize), head(0), tail(0) {
+Buffer::Buffer(int bufferSize, bool inputBuffer, string hierarchy) : size(bufferSize), head(0), tail(0)
+{
     data = new RecordStructure[size];
     inputBuffer = inputBuffer;
     hierarchy = hierarchy;
 }
 
-Buffer::~Buffer() {
+Buffer::~Buffer()
+{
     delete[] data;
 }
 
-bool Buffer::isEmpty() const {
+bool Buffer::isEmpty() const
+{
     return head == tail;
 }
 
-bool Buffer::isFull() const {
+bool Buffer::isFull() const
+{
     return (head + 1) % size == tail;
 }
 
-bool Buffer::write(RecordStructure value) {
-    if (!isFull()) {
+bool Buffer::write(RecordStructure value)
+{
+    if (!isFull())
+    {
         data[head] = value;
         head = (head + 1) % size;
         return true;
-    } else {
+    }
+    else
+    {
         evict(hierarchy);
     }
     return false; // Buffer is full
 }
 
-bool Buffer::read(RecordStructure &value) {
-    if (!isEmpty()) {
+bool Buffer::read(RecordStructure &value)
+{
+    if (!isEmpty())
+    {
         value = data[tail];
         tail = (tail + 1) % size;
         return true;
-    } else{
-        load(hierarchy);
+    }
+    else
+    {
+        // load(hierarchy);
+        printf("Loading in Read - Buffer.cpp\n");
     }
     return false; // Buffer is empty
 }
