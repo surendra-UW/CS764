6bcb946c654d73e61c3d3a492701f94257944540
Srihari <srihari961112@gmail.com>
2023-11-11 18:18:12 -0600

    Updated TournamentTree.cpp

--- TournamentTree.cpp
+++ TournamentTree.cpp
@@ -6,6 +6,7 @@
 #include <queue>
 #include "MinHeapNode.h"
 #include "RecordStructure.h"
+#include "TournamentTree.h"
 using namespace std;
 
 FILE *openFile(char *fileName, char *mode)
@@ -41,7 +42,7 @@ void createInitialRuns(char *input_file, int run_size, int num_ways)
     for (int i = 0; i < num_ways; i++)
     {
         char filename[50];
-        sprintf(filename, "output%d.txt", i);
+        sprintf(filename, "run%d.txt", i);
         out[i] = fopen(filename, "w");
         if (!out[i])
         {
@@ -63,7 +64,7 @@ void createInitialRuns(char *input_file, int run_size, int num_ways)
         int value1, value2, value3, value4;
         for (int i = 0; i < run_size; i++)
         {
-            if (fscanf(in, "%d,%d,%d,%d", &value1, &value2, &value3, &value4) != 3)
+            if (fscanf(in, "%d,%d,%d,%d", &value1, &value2, &value3, &value4) != 4)
             {
                 more_input = false;
                 break;
@@ -72,9 +73,8 @@ void createInitialRuns(char *input_file, int run_size, int num_ways)
             RecordStructure r = {value1, value2, value3, value4};
             arr.push_back(r);
         }
-        // readCSVRows(inp_file_string, run_size, arr);
-        sort(arr.begin(), arr.end(), compareByMember1); // do custom sort
-        cout << "Size of arr = " << arr.size() << endl;
+
+        sort(arr.begin(), arr.end(), compareByMember1);
 
         for (int j = 0; j < arr.size(); j++)
         {
@@ -98,35 +98,34 @@ void mergeFiles(char *output_file, int n, int k)
     for (int i = 0; i < k; i++)
     {
         char fileName[50];
-        snprintf(fileName, sizeof(fileName), "output%d.txt", i);
+        snprintf(fileName, sizeof(fileName), "run%d.txt", i);
         in[i] = openFile(fileName, "r");
     }
 
     FILE *out = openFile(output_file, "w");
-    int i, val1, val2, val3;
+    int i, val1, val2, val3, val4;
 
     priority_queue<MinHeapNode> pq;
 
     for (i = 0; i < k; i++)
     {
-        if (fscanf(in[i], "%d,%d,%d", &val1, &val2, &val3) != 3)
+        if (fscanf(in[i], "%d,%d,%d,%d", &val1, &val2, &val3, &val4) != 4)
             break;
-        // cout<<"Val ="<<val<<"\ti="<<i<<endl;
-        RecordStructure r = {val1, val2, val3};
+        RecordStructure r = {val1, val2, val3, val4};
         MinHeapNode record = {r, i};
         cout << "Adding element to priority queue\t" << record.element.member1 << "\t" << record.i << endl;
         pq.push(record);
     }
-    cout << "Size of priority queue " << pq.size() << endl;
+    // cout << "Size of priority queue " << pq.size() << endl;
     int count = 0;
 
     while (count != i)
     {
         MinHeapNode root = pq.top();
 
-        fprintf(out, "%d,%d,%d\n", root.element.member1, root.element.member2, root.element.member3);
+        fprintf(out, "%d,%d,%d,%d\n", root.element.member1, root.element.member2, root.element.member3, root.element.member4);
         cout << "Removed value of root from run " << root.i << " is " << root.element.member1 << endl;
-        if (fscanf(in[root.i], "%d,%d,%d", &val1, &val2, &val3) != 3)
+        if (fscanf(in[root.i], "%d,%d,%d", &val1, &val2, &val3, &val4) != 4)
         {
             root.element.member1 = INT_MAX;
             count++;
@@ -136,6 +135,7 @@ void mergeFiles(char *output_file, int n, int k)
             root.element.member1 = val1;
             root.element.member2 = val2;
             root.element.member3 = val3;
+            root.element.member4 = val4;
         }
         cout << "New value of root from run " << root.i << " is " << root.element.member1 << endl;
         pq.pop();
